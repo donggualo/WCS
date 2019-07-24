2f78dc5c121e92f9c3fda21efca38b77bc76337c
KYLE <kylemyworld@163.com>
2019-07-24 17:57:17 +0800

    edite From

--- WCS_phase1/Http/HttpControl.cs
+++ WCS_phase1/Http/HttpControl.cs
@@ -223,31 +223,37 @@ namespace WCS_phase1.Http
         /// <returns></returns>
         public string DoPost(string url)
         {
+            try
+            {
+                //定义request并设置request的路径
+                WebRequest request = WebRequest.Create(url);
 
-            //定义request并设置request的路径
-            WebRequest request = WebRequest.Create(url);
-
-            //定义请求的方式
-            request.Method = "POST";
+                //定义请求的方式
+                request.Method = "POST";
 
 
-            //定义response为前面的request响应
-            WebResponse response = request.GetResponse();
+                //定义response为前面的request响应
+                WebResponse response = request.GetResponse();
 
-            //获取相应的状态代码
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                //获取相应的状态代码
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
 
-            //定义response字符流
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();//读取所有
-            Console.WriteLine(responseFromServer);
+                //定义response字符流
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();//读取所有
+                Console.WriteLine(responseFromServer);
 
-            //关闭资源
-            reader.Close();
-            dataStream.Close();
-            response.Close();
-            return responseFromServer;
+                //关闭资源
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+                return responseFromServer;
+            }catch (Exception e)
+            {
+
+            }
+            return "";
         }
 
 
