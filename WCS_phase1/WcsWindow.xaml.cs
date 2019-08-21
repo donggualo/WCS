100328409539a784b6c7f1d2a803dadd3ef313c9
KYLE <kylemyworld@163.com>
2019-08-21 17:56:11 +0800

    NewWinAndStyle

---
+++ WCS_phase1/WcsWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Panuon.UI.Silver;
+
+namespace WCS_phase1
+{
+    /// <summary>
+    /// WcsWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class WcsWindow : WindowX
+    {
+        public WcsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            TreeViewItem item = sender as TreeViewItem;
+
+            Console.WriteLine(item.Header);
+        }
+    }
+}
