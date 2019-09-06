5ef4708cec0fc066ae5d6c627b936256ddeaa277
donggualo <646343920@qq.com>
2019-09-06 11:46:01 +0800

    update

--- WindowManager/W_ABC.xaml.cs
+++ WindowManager/W_ABC.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using ModuleManager.WCS;
+using Panuon.UI.Silver;
 using PubResourceManager;
 using TaskManager;
+using TaskManager.Devices;
 using WindowManager.Datagrid;
 
 namespace WindowManager
@@ -16,7 +20,7 @@ namespace WindowManager
     public partial class W_ABC : UserControl
     {
         private AbcDataGrid grid;
-        
+
 
         public W_ABC()
         {
@@ -36,9 +40,9 @@ namespace WindowManager
         private void getABCNameList()
         {
             List<WCS_CONFIG_DEVICE> list = CommonSQL.GetDeviceNameList(DataControl._mMySql, DeviceType.行车);
-            foreach(var l in list)
+            foreach (var l in list)
             {
-                grid.UpdateDeviceList(l.DEVICE,l.AREA);
+                grid.UpdateDeviceList(l.DEVICE, l.AREA);
             }
         }
 
@@ -55,36 +59,208 @@ namespace WindowManager
                         grid.UpdateDeviceList();
                     }));
                 }
-                
-            }catch(Exception e)
+
+            }
+            catch (Exception e)
             {
-                Console.WriteLine("更新终止："+e.Message);
+                Console.WriteLine("更新终止：" + e.Message);
             }
         }
 
+        /// <summary>
+        /// 限制仅输入数字
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InputNum(object sender, TextCompositionEventArgs e)
+        {
+            Regex re = new Regex("[^0-9]+");
+            e.Handled = re.IsMatch(e.Text);
+        }
+
+        /// <summary>
+        /// 定位
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void LocateBtn_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (CBdev.SelectedIndex == -1)
+                {
+                    Notice.Show("请选择设备！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                if (string.IsNullOrEmpty(xlocation.Text.Trim()) || string.IsNullOrEmpty(ylocation.Text.Trim()) || string.IsNullOrEmpty(zlocation.Text.Trim()))
+                {
+                    Notice.Show("请填写目的坐标！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                string dev = CBdev.Text;
 
+                ABC abc = new ABC(dev);
+                if (abc.ActionStatus() == ABC.Run)
+                {
+                    throw new Exception("设备运行中！");
+                }
+                if (abc.DeviceStatus() == FRT.DeviceError)
+                {
+                    throw new Exception("设备故障！");
+                }
+
+                int x = Convert.ToInt32(xlocation.Text.Trim());
+                int y = Convert.ToInt32(ylocation.Text.Trim());
+                int z = Convert.ToInt32(zlocation.Text.Trim());
+                byte[] order = ABC._TaskControl(ABC.TaskLocate, abc.ABCNum(), DataControl._mStools.IntToBytes(x), DataControl._mStools.IntToBytes(y), DataControl._mStools.IntToBytes(z));
+                if (!DataControl._mSocket.SendToClient(dev, order, out string result))
+                {
+                    throw new Exception(result);
+                }
+                Notice.Show("定位任务 指令发送成功！", "成功", 3, MessageBoxIcon.Success);
+            }
+            catch (Exception ex)
+            {
+                Notice.Show("指令发送失败：" + ex.ToString(), "错误", 3, MessageBoxIcon.Error);
+            }
         }
 
+        /// <summary>
+        /// 取货
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void LoadBtn_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (CBdev.SelectedIndex == -1)
+                {
+                    Notice.Show("请选择设备！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                if (string.IsNullOrEmpty(xlocation.Text.Trim()) || string.IsNullOrEmpty(ylocation.Text.Trim()) || string.IsNullOrEmpty(zlocation.Text.Trim()))
+                {
+                    Notice.Show("请填写目的坐标！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                string dev = CBdev.Text;
 
+                ABC abc = new ABC(dev);
+                if (abc.ActionStatus() == ABC.Run)
+                {
+                    throw new Exception("设备运行中！");
+                }
+                if (abc.DeviceStatus() == FRT.DeviceError)
+                {
+                    throw new Exception("设备故障！");
+                }
+
+                int x = Convert.ToInt32(xlocation.Text.Trim());
+                int y = Convert.ToInt32(ylocation.Text.Trim());
+                int z = Convert.ToInt32(zlocation.Text.Trim());
+                byte[] order = ABC._TaskControl(ABC.TaskTake, abc.ABCNum(), DataControl._mStools.IntToBytes(x), DataControl._mStools.IntToBytes(y), DataControl._mStools.IntToBytes(z));
+                if (!DataControl._mSocket.SendToClient(dev, order, out string result))
+                {
+                    throw new Exception(result);
+                }
+                Notice.Show("取货任务 指令发送成功！", "成功", 3, MessageBoxIcon.Success);
+            }
+            catch (Exception ex)
+            {
+                Notice.Show("指令发送失败：" + ex.ToString(), "错误", 3, MessageBoxIcon.Error);
+            }
         }
 
+        /// <summary>
+        /// 放货
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void UnloadBtn_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (CBdev.SelectedIndex == -1)
+                {
+                    Notice.Show("请选择设备！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                if (string.IsNullOrEmpty(xlocation.Text.Trim()) || string.IsNullOrEmpty(ylocation.Text.Trim()) || string.IsNullOrEmpty(zlocation.Text.Trim()))
+                {
+                    Notice.Show("请填写目的坐标！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                string dev = CBdev.Text;
 
+                ABC abc = new ABC(dev);
+                if (abc.ActionStatus() == ABC.Run)
+                {
+                    throw new Exception("设备运行中！");
+                }
+                if (abc.DeviceStatus() == FRT.DeviceError)
+                {
+                    throw new Exception("设备故障！");
+                }
+
+                int x = Convert.ToInt32(xlocation.Text.Trim());
+                int y = Convert.ToInt32(ylocation.Text.Trim());
+                int z = Convert.ToInt32(zlocation.Text.Trim());
+                byte[] order = ABC._TaskControl(ABC.TaskRelease, abc.ABCNum(), DataControl._mStools.IntToBytes(x), DataControl._mStools.IntToBytes(y), DataControl._mStools.IntToBytes(z));
+                if (!DataControl._mSocket.SendToClient(dev, order, out string result))
+                {
+                    throw new Exception(result);
+                }
+                Notice.Show("放货任务 指令发送成功！", "成功", 3, MessageBoxIcon.Success);
+            }
+            catch (Exception ex)
+            {
+                Notice.Show("指令发送失败：" + ex.ToString(), "错误", 3, MessageBoxIcon.Error);
+            }
         }
 
+        /// <summary>
+        /// 复位
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Relocate_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// 终止
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TerminateBtn_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (CBdev.SelectedIndex == -1)
+                {
+                    Notice.Show("请选择设备！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                string dev = CBdev.Text;
+                ABC abc = new ABC(dev);
+                if (abc.DeviceStatus() == FRT.DeviceError)
+                {
+                    throw new Exception("设备故障！");
+                }
 
+                byte[] order = ABC._StopTask(abc.ABCNum());
+                if (!DataControl._mSocket.SendToClient(dev, order, out string result))
+                {
+                    throw new Exception(result);
+                }
+                Notice.Show("终止任务 指令发送成功！", "成功", 3, MessageBoxIcon.Success);
+            }
+            catch (Exception ex)
+            {
+                Notice.Show("指令发送失败：" + ex.ToString(), "错误", 3, MessageBoxIcon.Error);
+            }
         }
     }
 }
--- WindowManager/W_FRT.xaml.cs
+++ WindowManager/W_FRT.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ModuleManager.WCS;
+using Panuon.UI.Silver;
 using PubResourceManager;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using TaskManager;
+using TaskManager.Devices;
 using WindowManager.Datagrid;
 
 namespace WindowManager
@@ -16,7 +18,7 @@ namespace WindowManager
     public partial class W_FRT : UserControl
     {
         private FrtDataGrid grid;
-        
+
 
         public W_FRT()
         {
@@ -25,7 +27,8 @@ namespace WindowManager
 
             DataContext = grid;
 
-            getABCNameList();
+            GetFRTNameList();
+            AddCombBox();
 
             new Thread(DoRefresh)
             {
@@ -33,13 +36,35 @@ namespace WindowManager
             }.Start();
         }
 
-        private void getABCNameList()
+        private void GetFRTNameList()
         {
-            List<WCS_CONFIG_DEVICE> list = CommonSQL.GetDeviceNameList(DataControl._mMySql,DeviceType.固定辊台);
-            foreach(var l in list)
+            List<WCS_CONFIG_DEVICE> list = CommonSQL.GetDeviceNameList(DataControl._mMySql, DeviceType.固定辊台);
+            foreach (var l in list)
             {
-                grid.UpdateDeviceList(l.DEVICE,l.AREA);
+                grid.UpdateDeviceList(l.DEVICE, l.AREA);
             }
+        }
+
+        private void AddCombBox()
+        {
+            // 辊台内容
+            //方式
+            CBsite1.Items.Add("启动1#辊台");
+            CBsite1.Items.Add("启动2#辊台");
+            CBsite1.Items.Add("启动全部辊台");
+            CBsite1.SelectedIndex = 0;
+            //方向
+            CBsite2.Items.Add("正向启动");
+            CBsite2.Items.Add("反向启动");
+            CBsite2.SelectedIndex = 0;
+            //类型
+            CBsite3.Items.Add("接货");
+            CBsite3.Items.Add("送货");
+            CBsite3.SelectedIndex = 0;
+            //数量
+            CBsite4.Items.Add("货物数量1");
+            CBsite4.Items.Add("货物数量2");
+            CBsite4.SelectedIndex = 0;
         }
 
         private void DoRefresh()
@@ -55,36 +80,147 @@ namespace WindowManager
                         grid.UpdateDeviceList();
                     }));
                 }
-                
-            }catch(Exception e)
+
+            }
+            catch (Exception e)
             {
-                Console.WriteLine("更新终止："+e.Message);
+                Console.WriteLine("更新终止：" + e.Message);
             }
         }
 
-        private void LocateBtn_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// 启动辊台
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BTNrun_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (CBdev.SelectedIndex == -1)
+                {
+                    Notice.Show("请选择设备！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                string dev = CBdev.Text;
+                FRT frt = new FRT(dev);
+                if (frt.ActionStatus() == FRT.Run)
+                {
+                    throw new Exception("设备运行中！");
+                }
+                if (frt.DeviceStatus() == FRT.DeviceError)
+                {
+                    throw new Exception("设备故障！");
+                }
 
+                // 方式
+                byte site1 = FRT.RollerRun1;
+                if (CBsite1.SelectedValue.ToString() == "启动2#辊台")
+                {
+                    site1 = FRT.RollerRun2;
+                }
+                if (CBsite1.SelectedValue.ToString() == "启动全部辊台")
+                {
+                    site1 = FRT.RollerRunAll;
+                }
+                // 方向
+                byte site2 = FRT.RunFront;
+                if (CBsite2.SelectedValue.ToString() == "反向启动")
+                {
+                    site2 = FRT.RunObverse;
+                }
+                // 类型
+                byte site3 = FRT.GoodsReceive;
+                if (CBsite3.SelectedValue.ToString() == "送货")
+                {
+                    site3 = FRT.GoodsDeliver;
+                }
+                // 数量
+                byte site4 = FRT.GoodsQty1;
+                if (CBsite4.SelectedValue.ToString() == "货物数量2")
+                {
+                    site4 = FRT.GoodsQty2;
+                }
+
+                byte[] order = FRT._RollerControl(frt.FRTNum(), site1, site2, site3, site4);
+                if (!DataControl._mSocket.SendToClient(dev, order, out string result))
+                {
+                    throw new Exception(result);
+                }
+                Notice.Show("停止辊台 指令发送成功！", "成功", 3, MessageBoxIcon.Success);
+            }
+            catch (Exception ex)
+            {
+                Notice.Show("指令发送失败：" + ex.ToString(), "错误", 3, MessageBoxIcon.Error);
+            }
         }
 
-        private void LoadBtn_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// 停止辊台
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BTNstop_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (CBdev.SelectedIndex == -1)
+                {
+                    Notice.Show("请选择设备！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                string dev = CBdev.Text;
+                FRT frt = new FRT(dev);
+                if (frt.DeviceStatus() == FRT.DeviceError)
+                {
+                    throw new Exception("设备故障！");
+                }
 
+                byte[] order = FRT._StopRoller(frt.FRTNum());
+                if (!DataControl._mSocket.SendToClient(dev, order, out string result))
+                {
+                    throw new Exception(result);
+                }
+                Notice.Show("停止辊台 指令发送成功！", "成功", 3, MessageBoxIcon.Success);
+            }
+            catch (Exception ex)
+            {
+                Notice.Show("指令发送失败：" + ex.ToString(), "错误", 3, MessageBoxIcon.Error);
+            }
         }
 
-        private void UnloadBtn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Relocate_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// 终止任务
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TerminateBtn_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (CBdev.SelectedIndex == -1)
+                {
+                    Notice.Show("请选择设备！", "提示", 3, MessageBoxIcon.Info);
+                    return;
+                }
+                string dev = CBdev.Text;
+                FRT frt = new FRT(dev);
+                if (frt.DeviceStatus() == FRT.DeviceError)
+                {
+                    throw new Exception("设备故障！");
+                }
 
+                byte[] order = FRT._StopTask(frt.FRTNum());
+                if (!DataControl._mSocket.SendToClient(dev, order, out string result))
+                {
+                    throw new Exception(result);
+                }
+                Notice.Show("终止任务 指令发送成功！", "成功", 3, MessageBoxIcon.Success);
+            }
+            catch (Exception ex)
+            {
+                Notice.Show("指令发送失败：" + ex.ToString(), "错误", 3, MessageBoxIcon.Error);
+            }
         }
     }
 }
